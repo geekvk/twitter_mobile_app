e7621413f6b90df1b1b495edfc0b989a747e945e
geekvk <vijanakaush@gmail.com>
2025-07-15 06:18:11 +0530

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+import "../global.css"
+
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="index" options={{ title : "Home"}}/>
+  </Stack>;
+}
